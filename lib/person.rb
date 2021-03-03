db68abe359382d9e835bf46c4ec419ed28209bdb
Mac Rowe <m.rowe42@gmail.com>
2021-03-02 19:28:03 -0500

    commit tree 9bcccd2dc7811e4e4493561c95948e7e4053a05c

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,85 @@
+require 'pry'
+
+
 # your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+    
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+
+    end
+
+    def happiness
+        @happiness.clamp(0, 10)
+    end
+
+    def hygiene
+        @hygiene.clamp(0, 10)
+    end 
+
+    def happy?
+       if @happiness > 7
+        return true
+       else
+        false
+       end
+    end 
+
+    def clean?
+        if @hygiene > 7
+            return true
+        else
+            false
+        end
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        return "♪ another one bites the dust ♫"
+    end 
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end 
+
+    def start_conversation(person, topic)
+        if topic == "politics"
+            person.happiness -= 2
+            self.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            person.happiness +=1
+            self.happiness +=1
+            return "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end 
+
+    
+    
+    end 
+
+
+
+
+
+end 
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require_relative "../lib/person.rb"
 
 RSpec.configure do |config|
-  
+
+    
+
 end
+
